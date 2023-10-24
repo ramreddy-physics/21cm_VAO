903abd8ee963e2e4ec63d916f20731534b4aaebc
ramreddy <ramreddy@utexas.edu>
2023-10-24 14:27:33 -0500

    10-24-2023

---
+++ kernel.py
@@ -0,0 +1,80 @@
+import torch
+import torch.nn as nn
+
+class Kernel:
+
+    def __init__(self, Model, X, Y)-> None:
+        self.Model=Model
+        self.lr=1.0e-04
+        self.eps=0.0
+        self.train_loader=None
+        self.val_loader=None
+        self.optimizer=torch.optim.Adam(Model.parameters(), lr=self.lr, eps=self.eps)
+        self.loss_fn=torch.nn.MSELoss()
+        self.batch_size=None
+        self.num_batches=None
+        self.split_train_val(X=X, Y=Y, val_fraction=0.1, batch_size=5)
+
+
+    def split_train_val(self, X, Y, val_fraction=0.1, batch_size=5)-> None:
+        N=X.shape[0]
+        N_train=int(N*(1-val_fraction))
+        self.train_loader=torch.utils.data.DataLoader([[X[i], Y[i]] for i in range(N_train)], batch_size=batch_size, shuffle=True)  
+        self.val_loader=torch.utils.data.DataLoader([[X[i], Y[i]] for i in range(N_train, N)], batch_size=batch_size, shuffle=True)
+        self.batch_size=batch_size
+        self.num_batches=int(N_train/batch_size)
+
+    def train_one_epoch(self):
+        running_loss = 0.
+        last_loss = 0.
+
+        for i, data in enumerate(self.train_loader):
+            inputs, labels = data
+
+            self.optimizer.zero_grad()
+
+            outputs = self.Model(inputs)
+
+            loss = self.loss_fn(outputs, labels)
+            loss.backward()
+
+            self.optimizer.step()
+
+            running_loss += loss.item()
+        
+            if (i+1) % self.num_batches == 0:
+                last_loss = running_loss/self.batch_size
+                running_loss = 0.
+
+        return last_loss
+    
+    def train_model(self, EPOCHS = 500):
+        epoch_number = 0
+        best_vloss = 1.0e+06
+
+        for epoch in range(EPOCHS):
+            self.Model.train(True)
+            avg_loss = self.train_one_epoch()
+
+            running_vloss = 0.0
+            self.Model.eval()
+
+            with torch.no_grad():
+                for i, vdata in enumerate(self.val_loader):
+                    vinputs, vlabels = vdata
+                    voutputs = self.Model(vinputs)
+                    vloss = self.loss_fn(voutputs, vlabels)
+                    running_vloss += vloss
+
+            avg_vloss = running_vloss / (i + 1)
+
+            if (epoch+1) % (EPOCHS/10) == 0:
+                print("EPOCH {}:".format(epoch + 1))
+                print("LOSS train {} valid {}".format(avg_loss, avg_vloss))
+
+        
+            if avg_vloss < best_vloss:
+                best_vloss = avg_vloss
+                torch.save(self.Model, "UNet_vcb.pt")
+
+        epoch_number += 1
--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import py21cmfast as p21c
 
 from py21cmfast import global_params
 from py21cmfast import plotting
+from kernel import Kernel
 
 import torch
 import torch.nn as nn
@@ -10,58 +11,15 @@ import torch.nn as nn
 from vcb_gen import generate_vcb
 from modules import UNet
 
-N_train = 100
-N = N_train
+N=100
+
 vcb, vcb_wr = generate_vcb(num_cubes=N)
-
-X_train = torch.tensor(
-    np.reshape(vcb_wr[:N_train], (N_train, 1, 16, 16, 16)), dtype=torch.float32
-)
-Y_train = torch.tensor(
-    np.reshape(vcb[:N_train], (N_train, 1, 16, 16, 16)), dtype=torch.float32
-)
-
-
-X_max = torch.max(X_train)
-X_min = torch.min(X_train)
-
-Y_max = torch.max(Y_train)
-Y_min = torch.min(Y_train)
-
-
-def normalize(T, T_max, T_min):
-    a = 2.0 / (T_max - T_min)
-    b = -(T_max + T_min) / (T_max - T_min)
-    return a * T + b
-
-
-def denormalize(T, T_max, T_min):
-    a = 2.0 / (T_max - T_min)
-    b = -(T_max + T_min) / (T_max - T_min)
-    return (T - b) / a
-
-
-channel_list = [1, 4, 4, 4, 8, 16]
+channel_list = [1, 4, 4]
 Model = UNet(channel_list=channel_list)
 
-N_epochs = 1500
-lr = 1.0e-04
-eps = 0.0
-loss = torch.nn.MSELoss()
-loss_list = []
+X = torch.tensor(np.reshape(vcb_wr, (N, 1, 16, 16, 16)), dtype=torch.float32)
+Y = torch.tensor(np.reshape(vcb, (N, 1, 16, 16, 16)), dtype=torch.float32)
 
-optimizer = torch.optim.Adam(Model.parameters(), lr=lr, eps=eps)
+Train_kernel=Kernel(Model=Model, X=X, Y=Y)
 
-for epoch in range(1, N_epochs + 1):
-    optimizer.zero_grad()
-    Y_pred = Model(X_train)
-    loss_val = loss(Y_train, Y_pred)
-    loss_val.backward()
-    optimizer.step()
-    loss_list.append(loss_val.detach().numpy())
-    if epoch % int(N_epochs / 10) == 0:
-        print(
-            "%d epochs completed, loss_value = %f" % (epoch, loss_list[-1]), flush=True
-        )
-
-torch.save(Model, "UNet_vcb.pt")
+Train_kernel.train_model(EPOCHS=1500)
